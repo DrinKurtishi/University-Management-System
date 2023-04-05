0cc3f2c3b96bd743a472e350b3e44a744e3160bf
Drin Kurtishi <kurtishidrin@gmail.com>
2023-04-05 15:55:39 +0200

    Cleaned up main and implemented sorting algorithm

--- Unisystem.cpp
+++ Unisystem.cpp
@@ -1,8 +1,28 @@
 /*
     University management system - a C++ project for University
+    Author: Drin Kurtishi
+    Author ID: 130814   
     Mentor: Prof. Lejla Abazi-Bexheti
-    Author: Drin Kurtishi 130814
-    Version: 1.0
+    Version: 1.0.0
+
+    This is a program that manages student records at a university. It uses a main menu with options to register new students, 
+    view all registered students, view students of a specific faculty, view university statistics, and end the program.
+
+    The program is written using the class Student, which serves as a blueprint for creating student objects. The class encapsulates
+    all the relevant information and behaviors of a student, making it easier to manage and manipulate student data. 
+
+    When registering a new student, the program generates a unique ID and stores it in a vector to avoid duplicates. It also sorts the student 
+    into the appropriate faculty vector and increments the number of students in that faculty.
+
+    The program uses a switch statement to determine which function to run based on the user's input.
+
+    The main program is structured with a do-while loop that continues to prompt the user for input until they choose to end the program. The program 
+    uses various helper functions to carry out specific tasks such as registering a new student, displaying all students, and displaying university statistics.
+
+    In addition to these features, the program also boasts a robust input validation system. Before any user input is processed, the program checks 
+    to ensure that it is in the correct format and meets any necessary requirements. This helps to prevent errors and ensure that the program runs smoothly. 
+    If the input is invalid, the program provides clear and helpful error messages to the user, guiding them through the correct input format. 
+    The input validation system is an important component of the program, ensuring that users are able to make the most of its features without encountering frustrating errors.
 
 */
 
@@ -33,10 +53,13 @@ class Student{
         //getters and setters for private variables
         std::string getName();
         void setName(std::string);
+
         std::string getSurname();
         void setSurname(std::string);
+
         std::string getID();
         void setID(std::string);
+
         std::string getFaculty();
         void setFaculty(std::string);
 
@@ -46,22 +69,31 @@ class Student{
 
         //methods for showing information
         void showStudentInfo();
-        void showStudentInfoFaculty();//used when displaying students of the same faculty (we dont need their faculty because we know it)
+        void showStudentInfoFaculty();//used when displaying only students of the same faculty (we dont need their faculty because we know it)
 };
+
+//Main helper functions for every switch case (these use the functions specified below)
+int showMainMenu();
+void registerStudent(std::vector<Student>&, int&, std::string&, std::vector<int>&, std::vector<Student>&, std::vector<Student>&, std::vector<Student>&, std::vector<Student>&, int&, int&, int&, int&, std::string, std::string);
+void displayAllStudents(std::vector<Student>&, int);
+void displayUniversityStatistics(int, int, int, int, int);
+void EndingProgramText(int);
 
 //functions for input validation
 bool validateAttribute(std::string);
 int CheckAndSetID(int, std::vector<int>);
 int ValidateChoice(std::string);
 
-//functions for simplifying main
+//functions for simplyfying
 std::string GetAndSetAttribute(std::string);
 int IDgenerator();
 std::string formatString(std::string, int);//formats users input(Uppercase letters followed by lowercase letters)
 std::string ChooseFaculty();
-void EndingProgramText(int);
+void chooseFacultyList(std::vector<Student>&, std::vector<Student>&, std::vector<Student>&, std::vector<Student>&, int&, int&, int&, int&);
 void showStudents(int, std::string);
-void chooseFacultyList(std::vector<Student>, std::vector<Student>, std::vector<Student>, std::vector<Student>, int, int, int, int);
+
+//function for sorting algorithm
+bool compareByName(Student&, Student&); //comparator used to tell std::sort to sort by student name
 
 int main()
 {
@@ -83,139 +115,34 @@ int main()
     std::vector<Student> LNstudent;//vector for language students
 
     //keeping track of the number of students of specific faculties
-    int i = 0;//used to increment student array/ nr of all students
+    int i = 0;//used to increment student vector and the number of all students
     int CSnumber = 0; //nr of Cs students
     int BEnumber = 0; //nr of BE students
     int LWnumber = 0; //nr of Law students
     int LNnumber = 0; //nr of Language students
 
     do{
-        std::cout << "\nPress 1 to register new student: \n";
-        std::cout << "\nPress 2 to view every student list: \n";
-        std::cout << "\nPress 3 to view student list of a specific faculty:\n";
-        std::cout << "\nPress 4 to view University statistics:\n";
-        std::cout << "\nPress 5 to end program(WARNING! This will erase your entire database)\n";
-        std::getline(std::cin, input);
-        system("CLS");//clear screen
-
-        choice = ValidateChoice(input);
+        choice = showMainMenu();
         
         switch(choice)
         { 
             case 1://registers new student
-                student.push_back(newStudent);//add blank student to vector
-                
-                student[i].setFaculty(ChooseFaculty());
-
-                do
-                {
-                    std::cout << "Enter Student Name: ";
-                    student[i].setName(GetAndSetAttribute(Name));
-                }
-                while(student[i].nameFlag == true);//if invalid, prompt again.
-    
-                do
-                {
-                    std::cout << "Enter Student Surname: ";
-                    student[i].setSurname(GetAndSetAttribute(Surname));
-                }
-                while(student[i].surnameFlag == true);//if invalid, prompt again.
-        
-                std::cout << "\nStudent " << student[i].getName() << " " << student[i].getSurname()//display student name after registering
-                          << " has been succesfuly registered!\n";
-
-                ID = std::to_string(CheckAndSetID(IDgenerator(), IDvector));//generate and validate ID so no duplicates are registered
-                student[i].setID(ID); //set unique ID to Student
-
-                //SORT MAIN STUDENT ARRAY
-
-                //choose in which faculty vector to put student
-                Faculty = student[i].getFaculty();
-                if(Faculty == "Computer sciences")
-                {
-                    CSstudent.push_back(student[i]);
-                    //SORT STUDENT ARRAY
-                    CSnumber++;//increment number of students in faculty
-                }
-                else if(Faculty == "Business economics")
-                {
-                    BEstudent.push_back(student[i]);
-                    //SORT STUDENT ARRAY
-                    BEnumber++;
-                }
-                else if(Faculty == "Law")
-                {
-                    LWstudent.push_back(student[i]);
-                    //SORT STUDENT ARRAY
-                    LWnumber++;
-                }
-                else
-                {
-                    LNstudent.push_back(student[i]);
-                    //SORT STUDENT ARRAY
-                    LNnumber++;
-                }
-                i++; //increment i to move to the next student in the array
+                registerStudent(student, i, ID, IDvector, CSstudent, BEstudent, LWstudent, LNstudent, CSnumber, BEnumber, LWnumber, LNnumber, Name, Surname);
             break;
 
-            case 2://shows list of all students
-                if(i != 0)
-                {
-                    for(int j = 0; j < i; j++)
-                    {
-                        std::cout << "Student " << j + 1 << ": \n";
-                        student[j].showStudentInfo();
-                    }   
-                }
-                else//if user chooses case 2 before registering any students execute this
-                {
-                   std::cout << "Please enter at least one student.\n";
-                }
+            case 2://displays all registered students
+                displayAllStudents(student, i);
             break; 
 
-            case 3:
-                chooseFacultyList(CSstudent, BEstudent, LWstudent, LNstudent, CSnumber, BEnumber, LWnumber, LNnumber);//show student list of specific faculty
+            case 3://displays students of specific faculty
+                chooseFacultyList(CSstudent, BEstudent, LWstudent, LNstudent, CSnumber, BEnumber, LWnumber, LNnumber);
             break;
 
-            case 4:
-                system("CLS");
-                std::cout << "You have selected to view University statistics!\n";
-                if(i == 0)
-                {
-                    std::cout << "\nThere are no students enrolled in your University\n";
-                }
-                else if(i == 1)
-                {
-                    std::cout << "\nThere is one student enrolled in your University ";             //if there is only one student in the program
-                    if(CSnumber == 1)                                                               //then check in which faculty it is and display it
-                    {
-                       std::cout << "and he/she is studying Computer science.\n";
-                    }
-                    else if(BEnumber == 1)
-                    {
-                       std::cout << "and he/she is studying Business economics\n";
-                    }
-                    else if(LWnumber == 1)
-                    {
-                        std::cout << "and he/she is studying Law\n";
-                    }
-                    else
-                    {
-                        std::cout << "and he/she is studying Languages\n";
-                    }
-                }
-                else
-                {
-                    std::cout << "\nThere are a total of " << i << " students enrolled in your University.\n"; //if theres more than one student
-                    showStudents(CSnumber, "Computer science");
-                    showStudents(BEnumber, "Business economics");
-                    showStudents(LWnumber, "Law");
-                    showStudents(LNnumber, "Language");
-                }
-               
+            case 4://displays some information about the University
+                displayUniversityStatistics(i, CSnumber, BEnumber, LWnumber, LNnumber);
             break;
 
-            case 5://Ends program
+            case 5://Terminates program
                 EndingProgramText(i);  
                 exitLoop = true;
             break; 
@@ -224,25 +151,130 @@ int main()
                 std::cout << "Invalid input, please try again.\n";
         }
     }while(exitLoop == false);
+    return 0;
 }
 
-//functions of student class
+//MAIN HELPER FUNCTIONS
 
-std::string formatString(std::string userInput, int inputLength)//function to format users input
+int showMainMenu() {
+    std::cout << "\nPress 1 to register new student: \n";
+    std::cout << "Press 2 to view every student list: \n";
+    std::cout << "Press 3 to view student list of a specific faculty:\n";
+    std::cout << "Press 4 to view University statistics:\n";
+    std::cout << "Press 5 to end program(WARNING! This will erase your entire database)\n";
+
+    std::string input;
+    std::getline(std::cin, input);
+    system("CLS");
+    return ValidateChoice(input);
+}
+
+void registerStudent(std::vector<Student>& student, int& i, std::string& ID, std::vector<int>& IDvector, std::vector<Student>& CSstudent, std::vector<Student>& BEstudent, std::vector<Student>& LWstudent, std::vector<Student>& LNstudent, int& CSnumber, int& BEnumber, int& LWnumber, int& LNnumber, std::string Name, std::string Surname)
 {
-    if(islower(userInput[0]))//capitalize first letter
-    {
-        userInput[0] = toupper(userInput[0]);
+    student.push_back(Student()); // add blank student to vector
+    
+    student[i].setFaculty(ChooseFaculty());
+
+    do {
+        std::cout << "Enter Student Name: ";
+        student[i].setName(GetAndSetAttribute(Name));
+    } while(student[i].nameFlag == true); // if invalid, prompt again.
+
+    do {
+        std::cout << "Enter Student Surname: ";
+        student[i].setSurname(GetAndSetAttribute(Surname));
+    } while(student[i].surnameFlag == true); // if invalid, prompt again.
+
+    std::cout << "\nStudent " << student[i].getName() << " " << student[i].getSurname() // display student name after registering
+              << " has been succesfuly registered!\n";
+
+    ID = std::to_string(CheckAndSetID(IDgenerator(), IDvector)); // generate and validate ID so no duplicates are registered
+    student[i].setID(ID); // set unique ID to Student
+
+    
+
+    // choose in which faculty vector to put student
+    std::string Faculty = student[i].getFaculty();
+    if(Faculty == "Computer sciences") {
+        CSstudent.push_back(student[i]);
+        CSnumber++; // increment number of students in faculty
+    } else if(Faculty == "Business economics") {
+        BEstudent.push_back(student[i]);
+        BEnumber++;
+    } else if(Faculty == "Law") {
+        LWstudent.push_back(student[i]);
+        LWnumber++;
+    } else {
+        LNstudent.push_back(student[i]);
+        LNnumber++;
     }
-    for(int i = 1; i < inputLength; i++)//i is 1 so it doesnt check first letter
-    {   
-        if(isupper(userInput[i]))                   //lowercase all uppercase       
-        {                                           //letters after first one
-            userInput[i] = tolower(userInput[i]);
+    i++; // increment i to move to the next student in the array
+    sort(student.begin(), student.end(), compareByName);//sort main student vector
+    //sort individual faculty vectors
+    sort(CSstudent.begin(), CSstudent.end(), compareByName);
+    sort(BEstudent.begin(), BEstudent.end(), compareByName);
+    sort(LWstudent.begin(), LWstudent.end(), compareByName);
+    sort(LNstudent.begin(), LNstudent.end(), compareByName);
+}
+void displayAllStudents(std::vector<Student>& student, int i) {
+    if (i != 0) {
+        for (int j = 0; j < i; j++) {
+            std::cout << "Student " << j + 1 << ":\n";
+            student[j].showStudentInfo();
         }
     }
-    return userInput;
+    else {
+        std::cout << "Please enter at least one student.\n";
+    }
 }
+
+void displayUniversityStatistics(int i, int CSnumber, int BEnumber, int LWnumber, int LNnumber) {
+    system("CLS");
+    std::cout << "You have selected to view University statistics!\n";
+    if (i == 0) {
+        std::cout << "\nThere are no students enrolled in your University.\n";
+    }
+    else if (i == 1) {
+        std::cout << "\nThere is one student enrolled in your University. ";
+        if (CSnumber == 1) {
+            std::cout << "He/she is studying Computer science.\n";
+        }
+        else if (BEnumber == 1) {
+            std::cout << "He/she is studying Business economics.\n";
+        }
+        else if (LWnumber == 1) {
+            std::cout << "He/she is studying Law.\n";
+        }
+        else {
+            std::cout << "He/she is studying Languages.\n";
+        }
+    }
+    else {
+        std::cout << "\nThere are a total of " << i << " students enrolled in your University.\n";
+        showStudents(CSnumber, "Computer science");
+        showStudents(BEnumber, "Business economics");
+        showStudents(LWnumber, "Law");
+        showStudents(LNnumber, "Language");
+    }
+}
+
+void EndingProgramText(int i)
+{
+    if(i != 0)//if there are registered students also print this
+    {
+        std::cout << "Erasing student database."; Sleep(600);
+        std::cout << "."; Sleep(600);
+        std::cout << "."; Sleep(600);
+        system("CLS");
+    }
+        std::cout<< "Terminating program."; Sleep(600);
+        std::cout<< "."; Sleep(600);
+        std::cout<< "."; Sleep(600);
+        system("CLS");
+        std::cout<< "Thank you for using Unisystem!\n"; Sleep(1000);
+}
+/////////////////////////////////////////////////////////////////////////////
+//FUNCTIONS FOR INPUT VALIDATION
 
 bool validateAttribute(std::string attribute) 
 {
@@ -265,11 +297,9 @@ bool validateAttribute(std::string attribute)
     return true; // if input is valid
 }
 
-//functions of main
-
 int CheckAndSetID(int random, std::vector<int> IDvector)
 {
-    //this code makes sure no duplicate ID is generated
+    //this loop makes sure no duplicate ID is generated
     while(std::count(IDvector.begin(), IDvector.end(), random))//if random is duplicate in ID vector,
     {                                                          //then generate a new ID until it is 
         random = IDgenerator();                                //unique
@@ -289,6 +319,8 @@ int ValidateChoice(std::string input)
         return 999;//set choice to an invalid input so default case is triggered(invalid)
     }
 }
+/////////////////////////////////////////////////////////////////////////////
+//FUNCTIONS FOR SIMPLYFYING
 
 std::string GetAndSetAttribute(std::string Attribute)
 {
@@ -301,6 +333,22 @@ int IDgenerator()
 {
     int n = 130000 + (rand() % 9999);
     return n;
+}
+
+std::string formatString(std::string userInput, int inputLength)//function to format users input
+{
+    if(islower(userInput[0]))//capitalize first letter
+    {
+        userInput[0] = toupper(userInput[0]);
+    }
+    for(int i = 1; i < inputLength; i++)//i is 1 so it doesnt check first letter
+    {   
+        if(isupper(userInput[i]))                   //lowercase all uppercase       
+        {                                           //letters after first one
+            userInput[i] = tolower(userInput[i]);
+        }
+    }
+    return userInput;
 }
 
 std::string ChooseFaculty()//switch case for choosing faculty of student
@@ -354,7 +402,7 @@ std::string ChooseFaculty()//switch case for choosing faculty of student
     return faculty;
 }
 
-void chooseFacultyList(std::vector<Student> CSstudent, std::vector<Student> BEstudent, std::vector<Student> LWstudent, std::vector<Student> LNstudent, int CSnumber, int BEnumber, int LWnumber, int LNnumber)
+void chooseFacultyList(std::vector<Student>& CSstudent, std::vector<Student>& BEstudent, std::vector<Student>& LWstudent, std::vector<Student>& LNstudent, int& CSnumber, int& BEnumber, int& LWnumber, int& LNnumber)
 {
     bool exitLoop = false;
     std::string input;
@@ -452,22 +500,6 @@ void chooseFacultyList(std::vector<Student> CSstudent, std::vector<Student> BEst
     }while (exitLoop == false);
 }
 
-void EndingProgramText(int i)
-{
-    if(i != 0)//if there are registered students also print this
-    {
-        std::cout << "Erasing student database."; Sleep(600);
-        std::cout << "."; Sleep(600);
-        std::cout << "."; Sleep(600);
-        system("CLS");
-    }
-        std::cout<< "Terminating program."; Sleep(600);
-        std::cout<< "."; Sleep(600);
-        std::cout<< "."; Sleep(600);
-        system("CLS");
-        std::cout<< "Thank you for using Unisystem!\n"; Sleep(1000);
-}
-
 void showStudents(int studentNumber, std::string faculty)
 {
     if(studentNumber == 0)//if there are no students in faculty
@@ -480,6 +512,10 @@ void showStudents(int studentNumber, std::string faculty)
     }
 }
 
+bool compareByName(Student& a, Student& b) { //comparator used for sorting algorithm
+    return a.getName() < b.getName();
+}
+/////////////////////////////////////////////////////////////////////////////
 //METHODS OF STUDENT CLASS
 
 std::string Student::getName()
